11e3e102d70391c718ce054e7a428cbbec5209f1
karryingyou <karryingyou@users.noreply.github.com>
2026-08-19 15:27:15 +0800

    Rename static image to jkpv-store.png and update templates

--- posSystem/settings.py
+++ posSystem/settings.py
@@ -116,6 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Added configuration so Django can find your downloaded Bootstrap files
 STATICFILES_DIRS = [
